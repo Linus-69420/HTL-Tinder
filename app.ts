b6204580adb9268a95bf5f525a427bf0dd9c5f0b
Linus <Linusnestler@gmail.com>
2023-03-06 18:52:26 +0100

    added userservice, moved files(not stable)

---
+++ app.ts
@@ -0,0 +1,35 @@
+import express, { response } from "express";
+import { join } from "path";
+import { UserSevice } from "./userService";
+
+const app = express();
+let userService = new UserSevice();
+
+app.get("/", (request, response) => {
+    const file: string = join(__dirname, "/public/index.html");
+    response.sendFile(file);
+});
+
+app.get("/:id", (request, response) => {
+    return JSON.stringify(userService.getUserById(Number(request.params.id)));
+});
+
+app.get("/all", (request, response) => {
+    return JSON.stringify(userService.getAllUsers());
+});
+
+app.post("/new:user", (request, response) => {
+    userService.addUser(JSON.parse(request.params.user));
+});
+
+app.put("/update:user", (request, response) => {
+    userService.updateUser(JSON.parse(request.params.user));
+});
+
+app.delete("/delete:id", (request, response) => {
+    userService.deleteUser(Number(request.params.id));
+});
+
+app.listen(3000, () => {
+    console.log("Server listening on port 3000");
+});
