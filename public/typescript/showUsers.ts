4a38757c44c71d7e0d86da20d491c219cf215b30
oliver-420 <oliver.daxinger@gmail.com>
2023-03-13 15:20:53 +0100

    updated "search-page"

--- public/typescript/showUsers.ts
+++ public/typescript/showUsers.ts
@@ -3,13 +3,6 @@ import { parse } from "querystring";
 import { parseArgs } from "util";
 import { IUser } from "./user";
 
-async function getUsers() {
-  await fetch("http://localhost:3000/htl/dating", {})
-    .then((response) => response.json())
-    .then((data) => {
-      createList(data, (<HTMLInputElement>document.getElementById("search-input")).value);
-    });
-}
 async function searchUsers() {
   await fetch("http://localhost:3000/htl/dating", {})
     .then((response) => response.json())
@@ -19,14 +12,16 @@ async function searchUsers() {
 }
 
 function createList(users: IUser[], name) {
+  let usersFound: Boolean = false;
   const searchResults = document.getElementById("search-results")!;
   searchResults.innerHTML = "";
   const ul = document.createElement("ul");
 
-  if (users.length > 0) {
+  if (users.length > 0 && name != "") {
     users.forEach(u => {
 
       if (u.name.toLocaleLowerCase().includes(name.toLocaleLowerCase())) {
+        usersFound = true;
         console.log(u);
         const li = document.createElement("li");
         li.textContent = u.name;
@@ -51,11 +46,39 @@ function createList(users: IUser[], name) {
           li.appendChild(pic);
         }
         ul.appendChild(li);
+
       }
     });
 
     searchResults.appendChild(ul);
-  } else {
+
+    const div1 = document.createElement("div");
+        div1.className = "ProfileItem";
+        div1.textContent = "Hallo";
+        const div2 = document.createElement("div");
+        div2.className = "ProfileImg";
+        const div3 = document.createElement("div");
+        div3.className = "imageContain";
+        const img = document.createElement("img");
+        img.src = "https://placehold.it/200x200";
+
+        div3.appendChild(img);
+        div2.appendChild(div3);
+
+        const div4 = document.createElement("div");
+        div4.className = "ProfileDes";
+        const a = document.createElement("a");
+        a.className = "profileTitle";
+        a.textContent = "Username";
+        const div5 = document.createElement("div");
+        div5.className = "profileInfo";
+        div4.append(a, div5);
+        div1.appendChild(div2);
+
+        searchResults.appendChild(div1);
+  } 
+  if(!usersFound){
+    searchResults.innerHTML = "";
     const p = document.createElement("p");
     p.textContent = "Keine Benutzer gefunden.";
     searchResults.appendChild(p);
